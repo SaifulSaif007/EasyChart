81b8ba452d09cb2ee6ac6c96c9424a535c25f074
Saiful Islam <saifulsaif321@gmail.com>
2025-01-08 00:24:20 +0600

    update gitignore & build sdk

--- app/src/main/java/com/saiful/easychart/MainActivity.kt
+++ app/src/main/java/com/saiful/easychart/MainActivity.kt
@@ -17,9 +17,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             EasyChartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
